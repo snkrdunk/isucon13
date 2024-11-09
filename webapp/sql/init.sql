9efdcc198da57d02c535450649a8d809a048de43
Masaya Hayashi <mcdonalds.only@gmail.com>
2024-11-09 18:34:24 +0900

    add 3 index

--- webapp/sql/init.sql
+++ webapp/sql/init.sql
@@ -1,12 +1,9 @@
 TRUNCATE TABLE themes;
-TRUNCATE TABLE icons;
 TRUNCATE TABLE reservation_slots;
 TRUNCATE TABLE livestream_viewers_history;
 TRUNCATE TABLE livecomment_reports;
-TRUNCATE TABLE ng_words;
 TRUNCATE TABLE reactions;
 TRUNCATE TABLE tags;
-TRUNCATE TABLE livecomments;
 TRUNCATE TABLE livestreams;
 TRUNCATE TABLE users;
 
@@ -15,6 +12,33 @@ CREATE TABLE `livestream_tags` (
   `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `livestream_id` BIGINT NOT NULL,
   `tag_id` BIGINT NOT NULL
+) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+
+DROP TABLE IF EXISTS `icons`;
+CREATE TABLE `icons` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `user_id` BIGINT NOT NULL,
+  `image` LONGBLOB NOT NULL
+) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+
+DROP TABLE IF EXISTS `ng_words`;
+CREATE TABLE `ng_words` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `user_id` BIGINT NOT NULL,
+  `livestream_id` BIGINT NOT NULL,
+  `word` VARCHAR(255) NOT NULL,
+  `created_at` BIGINT NOT NULL
+) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+CREATE INDEX ng_words_word ON ng_words(`word`);
+
+DROP TABLE IF EXISTS `livecomments`;
+CREATE TABLE `livecomments` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `user_id` BIGINT NOT NULL,
+  `livestream_id` BIGINT NOT NULL,
+  `comment` VARCHAR(255) NOT NULL,
+  `tip` BIGINT NOT NULL DEFAULT 0,
+  `created_at` BIGINT NOT NULL
 ) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 
 ALTER TABLE `themes` auto_increment = 1;
@@ -31,3 +55,6 @@ ALTER TABLE `livestreams` auto_increment = 1;
 ALTER TABLE `users` auto_increment = 1;
 
 ALTER TABLE `livestream_tags` ADD INDEX idx_01(livestream_id);
+ALTER TABLE `icons` ADD INDEX idx_02(user_id);
+ALTER TABLE `ng_words` ADD INDEX idx_04(user_id, livestream_id);
+ALTER TABLE `livecomments` ADD INDEX idx_05(livestream_id);
