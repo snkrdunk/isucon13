02455bdd3c2bab2d19ed16d0abca0912a59682a1
Masaya Hayashi <mcdonalds.only@gmail.com>
2024-11-09 18:13:12 +0900

    add index to livestream_tags

--- webapp/sql/init.sql
+++ webapp/sql/init.sql
@@ -6,10 +6,16 @@ TRUNCATE TABLE livecomment_reports;
 TRUNCATE TABLE ng_words;
 TRUNCATE TABLE reactions;
 TRUNCATE TABLE tags;
-TRUNCATE TABLE livestream_tags;
 TRUNCATE TABLE livecomments;
 TRUNCATE TABLE livestreams;
 TRUNCATE TABLE users;
+
+DROP TABLE IF EXISTS `livestream_tags`;
+CREATE TABLE `livestream_tags` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `livestream_id` BIGINT NOT NULL,
+  `tag_id` BIGINT NOT NULL
+) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 
 ALTER TABLE `themes` auto_increment = 1;
 ALTER TABLE `icons` auto_increment = 1;
@@ -23,3 +29,5 @@ ALTER TABLE `tags` auto_increment = 1;
 ALTER TABLE `livecomments` auto_increment = 1;
 ALTER TABLE `livestreams` auto_increment = 1;
 ALTER TABLE `users` auto_increment = 1;
+
+ALTER TABLE `livestream_tags` ADD INDEX idx_01(livestream_id);
